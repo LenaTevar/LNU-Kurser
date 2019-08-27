4856a2e6bda81590b946acd3004798d60ddaca18
Lena Tevar <13490448+LenaTevar@users.noreply.github.com>
2019-08-27 08:56:37 +0200

    Update Seconds.java

--- Kurser/1DV506/ht222fd_assign1/Seconds.java
+++ Kurser/1DV506/ht222fd_assign1/Seconds.java
@@ -24,3 +24,17 @@ public class Seconds {
 
     }
 }
+/*
+Hello, this is Lena from the future. 
+Don't hardcode numbers... that's not polite. 
+int oneMinuteSeconds = 60;
+int oneHourSeconds = 3600;
+
+then 
+int total = seconds + minutes * oneMinuteSeconds + hours * oneHourSeconds
+
+Actually, why not making a method? 
+public int timeToSeconds(int hours, int minutes, int seconds){
+    return seconds + minutes * oneMinuteSeconds + hours * oneHourSeconds;
+    }
+*/
